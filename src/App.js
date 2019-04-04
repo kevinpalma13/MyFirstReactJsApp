4c8778ca1cdd38bb5310e240dc42a3305fc6b579
System Administrator <root@MacBook-Pro-de-Kevin.local>
2019-04-04 19:34:41 +0200

    Second Commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   }
 
   render() {
-    const tareas = this.state.todos.map((todo, i) => {
+    const tareas = this.state.todos.map((todo,i) => {
       return (
         <div className="col-md-4" key={i}>
           <div className="card mt-4">
